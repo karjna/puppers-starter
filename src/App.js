d98cffd3ec3c2ce00f6dd84f952c24b2b9c03039
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-05-10 12:04:56 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Puppers App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
